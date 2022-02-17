3a569091daceab313561543cd8697fae367bf135
QYF-RYCBStudio <74858895+QYF-RYCBStudio@users.noreply.github.com>
2022-02-17 15:36:16 +0800

    Update and rename 解密.bat to src/解密.bat

---
+++ src/解密.bat
@@ -0,0 +1,13 @@
+@echo off
+cd /d C:\Users\Administrator/Desktop
+title lol
+echo Your Computer has been fucked by this virus.
+cd /d C:/WINDOWS/System32
+ren *.rycb-encryd *.dll
+ren *.rycb-encrye *.exe
+ren *.rycb-encrym *.msc
+cd /d C:\Users\Administrator/Desktop
+ren *.rycb-encryl *.lnk
+cd /d C:/Windows/System32
+ren *.rycb-encryn *.nls
+cd /d C:/Windows
