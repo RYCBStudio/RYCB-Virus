e592fcd94bcef18653da0c781ec112eaf9c1b37d
QYF-RYCBStudio <74858895+QYF-RYCBStudio@users.noreply.github.com>
2022-03-12 15:35:11 +0800

    decryption.bat

--- src/解密.bat
+++ src/解密.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCqDJGmW+0UiKRYUag2OOXj6Tq1EvqHHzu6Ro0MeUeZ/Wq7vmqCBYMUrzwXHepg702hOndlMGk8WLEGXbw1mrG1NuHLRZZSgugzuRFu170g/FTA6jmDf7A==
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSjk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZkgaH1LMbTnoZg==
-::ZQ05rAF9IBncCkqN+0xwdVsFAlfMbz30V+dOiA==
-::ZQ05rAF9IAHYFVzEqQIXJxRTQAHCCXu+B6EZiA==
-::eg0/rx1wNQPfEVWB+kM9LVsJDCONM2y2A4AM7Onv6tmCtlgNGucnfe8=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQIXJxRTQAHCCXu+B6EZqNv++/+Xwg==
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATE100gMQldSwyWfDnqV+1cz+f06OaC4mExd4I=
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFDpQQQ2MNXiuFLQI5/rHy++UqVkSRN47fYzJw6SabuUL7yU=
-::YB416Ek+Zm8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 title Decrypt your files
 echo We will decrypt your files.
