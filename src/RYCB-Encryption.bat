e3f557de92a1a1699b2264235e51ca5ad61d6088
QYF-RYCBStudio <74858895+QYF-RYCBStudio@users.noreply.github.com>
2022-03-12 15:34:05 +0800

    Update RYCB-Encryption.bat

--- src/RYCB-Encryption.bat
+++ src/RYCB-Encryption.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCqDJGmW+0UiKRYUag2OOXj6Tq1EvqHHzu6Ro0MeUeZ/Wq7vmqCBYMUrzwXHepg702hOndlMGk8WLEGXbw1mrG1NuHLRZZSgugzuRFu170g/FTQmyWrTg0s=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSzk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
-::ZQ05rAF9IBncCkqN+0xwdVsFAlfMbz30V+dOiA==
-::ZQ05rAF9IAHYFVzEqQIXJxRTQAHCCXu+B6EZiA==
-::eg0/rx1wNQPfEVWB+kM9LVsJDCONM2y2A4AM7Onv6tmCtlgNGucnfe8=
-::fBEirQZwNQPfEVWB+kM9LVsJDDa7H0n6I7sf+vHr++OIrA08RPJf
-::cRolqwZ3JBvQF1fEqQIXJxRTQAHCCXu+B6EZqNv++/+Xwg==
-::dhA7uBVwLU+EWGu912ADPA5QRQvi
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATE100gMQldSwyWfDnqVOdc2tHYzdmTt0kUW4I=
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCmDJBjEwksiPQgUDTjTADmGVYmaFAh+HwQDeq2bmCO3tUleK14B/AGcNqRrqXcR5EN5uos6BQlNbVotkO6xTLnES4QB6wMZWfRulqnA2gpxWllXj3HfgS4IZcd9io0GyyXe
-::YB416Ek+ZW8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 chcp 65001
 cls
